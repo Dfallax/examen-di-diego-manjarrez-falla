0c3922a8cc2c73fe055401791a8f03dc9ddfbb9d
Dfallax <dfalla0001@gmail.com>
2023-12-11 12:35:35 +0100

    diseño de la aplicacion terminada

--- diegomanjarrez/app/src/main/java/com/example/diegomanjarrez/SplashScreen.kt
+++ diegomanjarrez/app/src/main/java/com/example/diegomanjarrez/SplashScreen.kt
@@ -1,11 +1,18 @@
 package com.example.diegomanjarrez
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-    }
+        //openLogin()
+
+
+
+}
 }
